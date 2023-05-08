daaee363f98cb9683362881384932d36539f3660
kashinid <dima.kashin96@gmail.com>
2023-05-08 23:49:18 +0300

    Добавляет автоматизацию сборки

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import svgo from 'gulp-svgmin';
+import { deleteAsync } from 'del';
+import squoosh from 'gulp-libsquoosh';
+import terser from 'gulp-terser';
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+
 
 // Styles
 
@@ -12,10 +24,58 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+// HTML
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+export const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+}
+
+// Images
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png,svg}')
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('build/img'))
+}
+
+export const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png,svg}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest('build/img'));
+}
+
+//Svg-sprite
+const sprite = () => {
+  return gulp.src("source/img/social-network-icon/*.svg")
+    .pipe(svgo())
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 }
 
 // Server
@@ -23,7 +83,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +101,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, styles, scripts, copyImages, createWebp, sprite, server, watcher
 );
